8be55d1ee2d504a854aba2e4061036cb2bbe29c3
Eugene <evgromadko@gmail.com>
2021-03-18 21:13:44 +0300

    update maven config

--- src/main/java/by/epam/jwd/cyberbets/service/ResultStatusService.java
+++ src/main/java/by/epam/jwd/cyberbets/service/ResultStatusService.java
@@ -1,4 +1,4 @@
 package by.epam.jwd.cyberbets.service;
 
-public interface ResourceStatusService {
+public interface ResultStatusService {
 }
---
+++ src/main/java/by/epam/jwd/cyberbets/service/impl/ResultStatusServiceImpl.java
@@ -0,0 +1,6 @@
+package by.epam.jwd.cyberbets.service.impl;
+
+import by.epam.jwd.cyberbets.service.ResourceService;
+
+public class ResourceStatusServiceImpl implements ResourceService {
+}
